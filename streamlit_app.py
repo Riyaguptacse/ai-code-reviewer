beaad5ec5dce2fceb648de755e2b34b823dd555f
Riyaguptacse <71141493+Riyaguptacse@users.noreply.github.com>
2026-05-17 09:52:55 -0400

    update backend url

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,12 @@ st.title("🤖 AI Code Reviewer")
 language = st.selectbox("Select Language", ["Python", "JavaScript", "Java"])
 
 code = st.text_area("Paste your code here", height=300)
+
 def call_api():
     for i in range(3):
         try:
             r = requests.post(
-                "https://your-backend.onrender.com/review",
+                "https://ai-code-reviewer-1-n718.onrender.com/review",
                 json={
                     "language": language,
                     "code": code
